8ddfd01755f590ab896d8f81a80d81a65c2645d6
KienND <nguyenduckienuet@gmail.com>
2025-12-27 18:40:24 +0700

    add requirement.txt

--- utils_store/M04_Classification.py
+++ utils_store/M04_Classification.py
@@ -5,9 +5,6 @@ import numpy as np
 import pandas as pd
 import mne
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 from sklearn.linear_model import LogisticRegression
